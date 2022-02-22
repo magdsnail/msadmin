dc3f2233b4943fdfc60fabd52276a05f99a7dee0
jason.wang <wangkjim@gmail.com>
2022-02-22 19:22:41 +0800

    .

--- src/exceptions/api.exception.ts
+++ src/exceptions/api.exception.ts
@@ -20,4 +20,24 @@ export class ApiException extends HttpException {
   }
 }
 
+// import { HttpException } from '@nestjs/common';
+
+// export class ApiException extends HttpException {
+//   private errCode: number
+//   constructor(msg: string, errCode?: number) {
+//     //权限问题一律使用401错误码
+//     if (errCode && errCode == 401) {
+//       super(msg, 200)
+//       this.errCode = 401
+//     } else {
+//       //其他异常一律使用500错误码
+//       super(msg, errCode ?? 200)
+//       this.errCode = errCode ?? 500
+//     }
+//   }
+//   getErrCode(): number {
+//     return this.errCode
+//   }
+// }
+
 // 使用 throw new ApiException(1002)

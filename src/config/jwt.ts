c9b03aac1cc30bbba530980ed8ab99eace8eccca
jason.wang <wangkjim@gmail.com>
2022-02-25 17:11:23 +0800

    .

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -2,5 +2,6 @@ import { registerAs } from '@nestjs/config'
 
 export default registerAs('JWT', () => ({
   secretKey: process.env.JWT_SECRET_KEY || 'heluoyun_manage',
-  expiresIn: process.env.JWT_EXPIRESIN || '24h'
+  expiresIn: process.env.JWT_EXPIRESIN || '24h',
+  redisExpires: Number(process.env.JWT_REDISEXPIRES || 86400)
 }));
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -61,15 +61,15 @@ export class UserService {
         const expires = this.configService.get<number>('JWT.redisExpires');
         //开发环境 复用 token，取消单点登录
         if (process.env.NODE_ENV == 'development') {
-            const tagtoken = await this.redisService.getRedis('admin').get(`${USER_TOKEN_KEY}:${user.user_id}`)
+            const tagtoken = await this.redisService.getRedis('admin').get(`${USER_TOKEN_KEY}:${user.id}`)
             if (tagtoken) {
                 token = tagtoken
             }
         }
         //存储密码版本号，防止登录期间 密码被管理员更改后 还能继续登录
-        await this.redisService.getRedis('admin').set(`${USER_VERSION_KEY}:${user.user_id}`, 1)
+        await this.redisService.getRedis('admin').set(`${USER_VERSION_KEY}:${user.id}`, 1)
         //存储token, 防止重复登录问题，设置token过期时间(1天后 token 自动过期)，以及主动注销token。
-        await this.redisService.getRedis('admin').set(`${USER_TOKEN_KEY}:${user.user_id}`, token, 'EX', expires);
+        await this.redisService.getRedis('admin').set(`${USER_TOKEN_KEY}:${user.id}`, token, 'EX', expires);
         return {
             info: {
                 token

06531aa8453d6cfa63fd2adcb47798af1e3f7235
jason.wang <wangkjim@gmail.com>
2022-03-31 15:20:54 +0800

    。。

--- src/exceptions/api.exception.ts
+++ src/exceptions/api.exception.ts
@@ -10,9 +10,16 @@ export class ApiException extends HttpException {
    */
   private errorCode: number;
 
-  constructor(errorCode: number, msg?: any) {
-    super(msg ?? ErrorCodeMap[errorCode], 200);
-    this.errorCode = errorCode;
+  // constructor(errorCode: number, msg?: any) {
+  //   super(msg ?? ErrorCodeMap[errorCode], 200);
+  //   this.errorCode = errorCode;
+  // }
+
+  constructor(msg: any, errCode?: any) {
+    const message = typeof msg == 'string' ? msg : ErrorCodeMap[msg] ?? '系统出错';
+    super(message, 200);
+    // this.errorCode = errCode;
+    this.errorCode = typeof msg == 'string' ? errCode ?? 500 : msg
   }
 
   getErrorCode(): number {
---
+++ src/guard/demo-environment.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { ApiException } from '../exceptions/api.exception';
+
+@Injectable()
+export class DemoEnvironmentGuard implements CanActivate {
+  constructor() { }
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const isDemoEnvironment = process.env.NODE_ENV !== 'production';
+    if (isDemoEnvironment) return true;
+    const request: Request = context.switchToHttp().getRequest()
+    const allowUrlArr = ['/login', '/logout']   //放过的路由
+    if (request.method.toLocaleLowerCase() != 'get' && !allowUrlArr.includes(request.url))
+      throw new ApiException('演示环境,不允许操作')
+    return true
+  }
+}

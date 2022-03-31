b9a1c233c307a1523abe94d025217a16467601ba
jason.wang <wangkjim@gmail.com>
2022-03-31 18:11:55 +0800

    请求限制

--- src/decorator/constant.ts
+++ src/decorator/constant.ts
@@ -1,3 +1,6 @@
 export const IS_SKIPAUTH_KEY = 'skipAuth';
 
 export const ROLES_KEY = 'roles';
+
+/* 防止重复提交 */
+export const REOEATSUBMIT_METADATA = "repeatSubmit"
---
+++ src/guard/repeat-submit.guard.ts
@@ -0,0 +1,38 @@
+import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { REOEATSUBMIT_METADATA } from '../decorator/constant';
+import { RepeatSubmitOption } from '../decorator/repeat-submit.decorator';
+import { Request } from 'express'
+import { ApiException } from '../exceptions/api.exception';
+import { RedisService } from '../dynamic/redis/redis.service';
+
+@Injectable()
+export class RepeatSubmitGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly redisService: RedisService,
+    @InjectRedis() private readonly redis: Redis
+  ) { }
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const repeatSubmitOption: RepeatSubmitOption = this.reflector.get(REOEATSUBMIT_METADATA, context.getHandler())
+    if (!repeatSubmitOption) return true
+
+    const request: Request = context.switchToHttp().getRequest()
+    const cache = await this.redisService.getRedis('admin').get(request.url);
+    const data = { body: request.body, prams: request.params, query: request.query }
+    const dataString = JSON.stringify(data)
+    if (!cache) {   //没有缓存数据
+      if (dataString) {
+        await this.redisService.getRedis('admin').set(request.url, dataString, 'EX', repeatSubmitOption.interval)
+      }
+    } else {
+      if (dataString && cache === dataString) {
+        throw new ApiException(repeatSubmitOption.message)
+      }
+    }
+    return true
+  }
+}
